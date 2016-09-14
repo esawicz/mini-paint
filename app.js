355402ef2ea0f44a2efdf335eee9b7622c2c7481
Ellen Sawicz <ellensawicz@Ellens-MacBook-Pro.local>
2016-09-13 18:59:49 -0600

    completed

---
+++ app.js
@@ -0,0 +1,51 @@
+  $(document).ready(function() {
+
+  	var color = 'white';
+
+  	$('.box').on('click', function() {
+    	$(this).addClass(color);
+  	})
+  
+  	$('.box').on('dblclick', function() {
+    	$(this).removeClass(color);
+  	})
+
+  	$('#reset').on('click', function() {
+  		$('.box').removeClass(color);
+  	})
+
+  	$('#red').on('click', function() {
+  		color = 'red';
+  	})
+
+  	$('#green').on('click', function() {
+  		color = 'green';
+  	})
+
+  	$('#blue').on('click', function() {
+  		color = 'blue';
+  	})
+
+  	$('#yellow').on('click', function() {
+  		color = 'yellow';
+  	})
+
+  	$('#white').on('click', function() {
+  		color = 'white';
+  	})
+
+var colors = 'white green red blue yellow'
+
+ 
+  $('.box').on('dblclick', function() {
+    $(this).removeClass(colors);
+  })
+
+  $('#reset').on('click', function() {
+    $('.box').removeClass(colors)
+  })
+
+
+
+
+  })
